af175a76f54ccb8b7dfbdf60c8412048891375d1
MateuszSzy <tasior19xx@gmail.com>
2022-02-05 17:30:13 +0100

    exercise first - basic

---
+++ exercise 1/heroes.js
@@ -0,0 +1,48 @@
+class Hero {
+    #powerCount = 10;
+
+    constructor(name) {
+        this.name = name;
+    }
+
+    getCurrentPowerCount() {
+        return this.#powerCount;
+    }
+
+    train() {
+        this.#powerCount + 1;
+    }
+}
+
+let heroCount = 0;
+
+const heroes = [];
+
+let hero = gethero("hulk");
+let geralt = gethero("geralt");
+
+heroes.push(hero);
+heroes.push(geralt);
+
+print_halt_of_name();
+
+function gethero() {
+    heroCount++;
+    let hero = new Hero('hulk');
+}
+
+const print_hall_of_fame = () => {
+    let spiderman = gethero(spider);
+    heroes.push(spiderman);
+
+    const heroesContainer = document.querySelector("#div");
+
+    heroes.map((x, i, arr) => {
+        const heroElement = document.createElement("div");
+        const [name] = x;
+
+        heroElement.innerHTML = `<p>${name}</p>${x.#powerCount}</p>`;
+
+        heroesContainer.append(heroElement);
+    });
+};
